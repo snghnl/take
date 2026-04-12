75608745afe23cabecdd25beb606ce40cb5b8cb0
snghnl <viselacity@gmail.com>
2026-04-12 22:55:04 +0900

    chore: use tailwindcss

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'wxt';
+import tailwindcss from '@tailwindcss/vite';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
   modules: ['@wxt-dev/module-react'],
   manifest: {
     permissions: ['storage', 'activeTab', 'sidePanel', 'scripting', 'tabs'],
